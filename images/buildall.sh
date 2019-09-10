33c4213bf2d8e4aa45687ff84fbb031d8626cf2a
Iman Akbari <imakbari@gmail.com>
2019-09-10 23:41:52 +0430

    Cleanup Docker scripts

---
+++ images/buildall.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DIR=$(realpath $(dirname $0))
+
+set -e
+
+echo Build base
+cd $DIR/base
+./build
+
+echo Build host-base
+cd $DIR/host-base
+./build
+
+echo Build gateway base
+cd $DIR/gw-base
+./build
+
+echo Build IDS
+cd $DIR/ids
+./build
+
+
