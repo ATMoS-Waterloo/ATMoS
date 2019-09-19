1ffbd6c72473a5c7c168eb661b5223d26c6d5279
Iman Akbari <imakbari@gmail.com>
2019-09-19 02:25:25 -0400

    Update IPS rules

--- images/benign-googler/entry.sh
+++ images/benign-googler/entry.sh
@@ -6,6 +6,8 @@ DIR=$(dirname $0)
 sudo route add default gw $(ifconfig $(cat /etc/hostname)-eth0 | grep -oE "inet addr:([0-9].?)+" | grep -oE "([0-9]+\.)+")200
 sudo route del default gw $(ifconfig eth0 | grep -oE "inet addr:([0-9].?)+" | grep -oE "([0-9]+\.)+")1
 
+screen -dmSL pinger ping 8.8.8.8
+
 # Run Googler
 bash src/dispatch.sh google benign
 
---
+++ images/benign-googler/pinger.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+while [ true ]
+do
+  ping -c5 8.8.8.8
+  sleep 5
+done
