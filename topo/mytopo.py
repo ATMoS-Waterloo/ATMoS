f9456152b86cf470e189982974552d629d1d184a
Iman Akbari <imakbari@gmail.com>
2019-09-22 14:48:48 -0400

    9 host APT

--- topo/mytopo.py
+++ topo/mytopo.py
@@ -33,10 +33,13 @@ h1 = net.addDocker('h1', ip='210.0.0.101', mac="00:00:00:00:00:03", dimage="mg-b
 h2 = net.addDocker('h2', ip='210.0.0.102', mac="00:00:00:00:00:04", dimage="mg-benign-googler")
 h3 = net.addDocker('h3', ip='210.0.0.103', mac="00:00:00:00:00:05", dimage="mg-benign-googler")
 h4 = net.addDocker('h4', ip='210.0.0.104', mac="00:00:00:00:00:06", dimage="mg-benign-googler")
+h5 = net.addDocker('h5', ip='210.0.0.105', mac="00:00:00:00:00:07", dimage="mg-benign-googler")
 
 info('*** Adding simulations (malicious)\n')
-m1 = net.addDocker('m1', ip='210.0.0.110', mac="00:00:00:00:00:10", dimage="mg-malish-syn")
-m2 = net.addDocker('m2', ip='210.0.0.111', mac="00:00:00:00:00:11", dimage="mg-malish-syn")
+m1 = net.addDocker('m1', ip='210.0.0.110', mac="00:00:00:00:00:10", dimage="mg-malish-apt")
+m2 = net.addDocker('m2', ip='210.0.0.111', mac="00:00:00:00:00:11", dimage="mg-malish-apt")
+m3 = net.addDocker('m3', ip='210.0.0.112', mac="00:00:00:00:00:12", dimage="mg-malish-apt")
+m4 = net.addDocker('m4', ip='210.0.0.113', mac="00:00:00:00:00:13", dimage="mg-malish-apt")
 
 info('*** Adding switches\n')
 
@@ -45,9 +48,9 @@ s2 = net.addSwitch('sw2')
 
 info('*** Creating links\n')
 
-s1_hosts = (h1, h3, m1, gw1)
+s1_hosts = (h1, h3, h5, m1, m3, gw1)
 # s1_hosts = (h1, m1, gw1)
-s2_hosts = (h2, h4, m2, gw2)
+s2_hosts = (h2, h4, m2, m4, gw2)
 # s2_hosts = (h2, gw2)
 
 for x in s1_hosts:
